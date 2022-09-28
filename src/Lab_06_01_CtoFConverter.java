6c7e3bbf226564a2f8ea125dc6b876ee4b7dc530
Rudi <vogelrj@mail.uc.edu>
2022-09-27 23:51:32 -0400

    Final build

--- src/Lab_06_01_CtoFConverter.java
+++ src/Lab_06_01_CtoFConverter.java
@@ -1,20 +1,29 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Lab_06_01_CtoFConverter {
     public static void main(String[] args) {
+        String trash = "";
         double celTemp = 0;
         double fahTemp = 0;
         int boilPoint = 212;
         int freezPoint = 32;
-
+        DecimalFormat df = new DecimalFormat("0.00");
         Scanner in = new Scanner(System.in);
         System.out.print("What is the temperature in Celsius? ");
-        celTemp = in.nextDouble();
-        fahTemp = (celTemp * 1.8) + 32;
-        if (fahTemp >= boilPoint)
-        {
-            System.out.println("The temperature in Fahrenheit is " + fahTemp + ". That is boiling.");
+        if (in.hasNextDouble()) {
+            celTemp = in.nextDouble();
+            fahTemp = (celTemp * 1.8) + 32;
+            if (fahTemp >= boilPoint) {
+                System.out.println("The temperature in Fahrenheit is " + df.format(fahTemp) + ". That is boiling.");
+            } else if (fahTemp <= freezPoint) {
+                System.out.println("The temperature in Fahrenheit is " + df.format(fahTemp) + ". That is freezing.");
+            } else {
+                System.out.println("The temperature in Fahrenheit is " + df.format(fahTemp) + ".");
+            }
         }
-
+        else {
+            System.out.println("Invalid Entry. Please try again");
+        }
     }
 }
