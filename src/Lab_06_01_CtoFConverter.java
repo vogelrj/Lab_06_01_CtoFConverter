73eab3e75c35e078495f84fa9862cf98c1d20f6c
Rudi <vogelrj@mail.uc.edu>
2022-10-10 14:01:57 -0400

    updated build

--- src/Lab_06_01_CtoFConverter.java
+++ src/Lab_06_01_CtoFConverter.java
@@ -3,17 +3,31 @@ import java.util.Scanner;
 
 public class Lab_06_01_CtoFConverter {
     public static void main(String[] args) {
-        String trash = "";
+        boolean blockVal = true;
+        boolean newGame = false;
+        String GameResp = "";
+        String TrashBag= "";
         double celTemp = 0;
         double fahTemp = 0;
         int boilPoint = 212;
         int freezPoint = 32;
         DecimalFormat df = new DecimalFormat("#.00");
         Scanner in = new Scanner(System.in);
-        System.out.print("What is the temperature in Celsius? ");
-        if (in.hasNextDouble()) {
-            celTemp = in.nextDouble();
-            in.nextLine();
+        do {
+            do {
+                System.out.print("What is the temperature in Celsius? ");
+                if (in.hasNextDouble()) {
+                    celTemp = in.nextDouble();
+                    in.nextLine();
+                    blockVal = true;
+                }
+                else {
+                    System.out.println("Invalid input. Please try again.");
+                    TrashBag = in.nextLine();
+                    blockVal = false;
+                }
+            }
+            while (!blockVal);
             fahTemp = (celTemp * 1.8) + 32;
             if (fahTemp >= boilPoint) {
                 System.out.println("The temperature in Fahrenheit is " + df.format(fahTemp) + ". That is boiling.");
@@ -24,10 +38,24 @@ public class Lab_06_01_CtoFConverter {
             else {
                 System.out.println("The temperature in Fahrenheit is " + df.format(fahTemp) + ".");
             }
+            System.out.println("Do you want to convert another number? [Y/N]");
+            do {
+                GameResp = in.nextLine();
+                if (GameResp.equalsIgnoreCase("y")) {
+                    newGame = true;
+                    blockVal = true;
+                }
+                else if (GameResp.equalsIgnoreCase("n")) {
+                    System.out.println("Thanks for your time!");
+                    System.exit(0);
+                }
+                else {
+                    System.out.println("Invalid Entry. Please Select [Y/N]");
+                    blockVal = false;
+                }
+            }
+            while (!blockVal);
         }
-        else {
-            trash.equals(in.nextLine());
-            System.out.println("Invalid Entry. Please try again");
-        }
+        while (newGame =  true);
     }
 }
